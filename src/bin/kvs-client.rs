f5aa80302079bda387e9a13af5ac87090e96ef1e
Nik Bond <nikbond97@gmail.com>
2020-01-20 21:11:56 +0200

    Handle server error in client

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -4,7 +4,8 @@ use log::{debug, info, warn, error};
 use std::io::{Write, Read, BufWriter, BufReader};
 use structopt::StructOpt;
 
-use kvs::{Request, Response};
+use kvs::{Request, Response, KvError};
+use std::process::exit;
 
 const DEFAULT_SERVER_ADDRESS: &'static str = "127.0.0.1:4000";
 
@@ -20,7 +21,6 @@ struct ClientArgs {
 
     #[structopt(subcommand)]
     cmd: Command,
-
 }
 
 #[derive(Debug, StructOpt)]
@@ -37,8 +37,7 @@ enum Command {
     }
 }
 
-
-fn request(server_addr: SocketAddr, req: Request) {
+fn request(server_addr: SocketAddr, req: Request) -> Response {
     debug!("Trying to connect to server at {}", server_addr);
 
     let mut stream = TcpStream::connect(server_addr).unwrap();
@@ -48,23 +47,53 @@ fn request(server_addr: SocketAddr, req: Request) {
     debug!("Send request: {:?}", req);
     serde_json::to_writer(&mut writer, &req).unwrap();
     writer.flush().unwrap();
-    let response: Response = serde_json::from_reader(reader).unwrap();
-    debug!("Response: {:?}", response);
+    serde_json::from_reader(reader).unwrap()
 }
 
 fn get(server_addr: SocketAddr, key: String) {
     let req = Request::Get {key};
-    request(server_addr, req);
+    let response = request(server_addr, req);
+    debug!("Response: {:?}", response);
+    match response {
+        Response::Ok(option_value) => {
+            match option_value {
+                Some(value) => println!("{}", value),
+                None => println!("{}", KvError::KeyNotFound),
+            }
+        },
+        Response::Err(e) => {
+            error!("{}", e);
+            exit(-1);
+        }
+    }
 }
 
 fn set(server_addr: SocketAddr, key: String, value: String) {
     let req = Request::Set {key, value};
-    request(server_addr, req);
+    let response = request(server_addr, req);
+    debug!("Response: {:?}", response);
+    if let Response::Err(e) = response {
+        error!("{}", e);
+        exit(-2);
+    }
 }
 
 fn rm(server_addr: SocketAddr, key: String) {
     let req = Request::Rm {key};
-    request(server_addr, req);
+    let response = request(server_addr, req);
+    debug!("Response: {:?}", response);
+    match response {
+        Response::Ok(_) => {},
+        Response::Err(what) => {
+            if what == format!("{}", KvError::KeyNotFound) {
+                eprintln!("{}", KvError::KeyNotFound); //todo use KvError instead of String
+            }
+            else {
+                error!("{}", what);
+                exit(-3);
+            }
+        }
+    }
 }
 
 fn main() {
