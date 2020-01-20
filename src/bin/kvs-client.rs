ec58eb3ea202eaf9ed29fc4574ff390d8eeb5d2f
Nik Bond <nikbond97@gmail.com>
2020-01-20 21:29:55 +0200

    Handle clients error

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -3,6 +3,7 @@ use simplelog::*;
 use log::{debug, info, warn, error};
 use std::io::{Write, Read, BufWriter, BufReader};
 use structopt::StructOpt;
+use failure::Fail;
 
 use kvs::{Request, Response, KvError};
 use std::process::exit;
@@ -37,17 +38,63 @@ enum Command {
     }
 }
 
-fn request(server_addr: SocketAddr, req: Request) -> Response {
-    debug!("Trying to connect to server at {}", server_addr);
+#[derive(Fail, Debug)]
+enum ClientError { //todo avoid duplicate with ServerError
+    #[fail(display = "IO Error: {}", _0)]
+    IoError(#[cause] std::io::Error),
 
-    let mut stream = TcpStream::connect(server_addr).unwrap();
+    #[fail(display = "Serde Error: {}", _0)]
+    SerdeError(#[cause] serde_json::Error),
+
+    #[fail(display = "Unknown Error: {}", _0)]
+    UnknownError(String),
+}
+
+impl From<std::io::Error> for ClientError {
+    fn from(err: std::io::Error) -> ClientError {
+        let res = ClientError::IoError(err);
+        error!("{}", res);
+        res
+    }
+}
+
+impl From<serde_json::Error> for ClientError {
+    fn from(err: serde_json::Error) -> ClientError {
+        let res = ClientError::SerdeError(err);
+        error!("{}", res);
+        res
+    }
+}
+
+impl From<String> for ClientError {
+    fn from(err: String) -> ClientError {
+        let res = ClientError::UnknownError(err);
+        error!("{}", res);
+        res
+    }
+}
+
+fn request(server_addr: SocketAddr, req: Request) -> Response {
+    match request_impl(server_addr, req) {
+        Ok(response) => return response,
+        Err(e) => {
+            error!("{}", e);
+            exit(-4);
+        }
+    }
+}
+
+fn request_impl(server_addr: SocketAddr, req: Request) -> Result<Response, ClientError> {
+    debug!("Request: {:?}", req);
+    debug!("Trying to connect to server at {}", server_addr);
+    let mut stream = TcpStream::connect(server_addr)?;
     let reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
-    debug!("Client started at {}", stream.local_addr().unwrap());
+    debug!("Client started at {}", stream.local_addr()?);
     debug!("Send request: {:?}", req);
-    serde_json::to_writer(&mut writer, &req).unwrap();
-    writer.flush().unwrap();
-    serde_json::from_reader(reader).unwrap()
+    serde_json::to_writer(&mut writer, &req)?;
+    writer.flush()?;
+    Ok(serde_json::from_reader(reader)?)
 }
 
 fn get(server_addr: SocketAddr, key: String) {
