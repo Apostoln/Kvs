dd4841c26af1f95cce4289c0f6379008c72bb6f3
Nik Bond <nikbond97@gmail.com>
2020-01-22 20:43:10 +0200

    Handle changing engine in current directory

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,14 +1,17 @@
 use std::net::SocketAddr;
 use std::env;
+use std::process::exit;
+
 use structopt::StructOpt;
 use structopt::clap::arg_enum;
 use simplelog::*;
-use log::error;
+use log::{debug, error};
 
 use kvs::{KvStore, SledEngine, KvsEngine, Result};
 use kvs::Server;
 
 const DEFAULT_ADDRESS: &'static str = "127.0.0.1:4000";
+const ENGINE_PATH: &'static str = "engine";
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "kvs-server")]
@@ -44,26 +47,58 @@ arg_enum! {
     }
 }
 
+fn current_engine<T>(path: T) -> Option<Engine>
+    where
+        T: Into<std::path::PathBuf>,
+{
+    let path = path.into();
+    if !path.exists() {
+        return None;
+    }
+
+    Some(std::fs::read_to_string(path)
+        .expect("Error reading from engine file")
+        .parse()
+        .expect("The content of engine file is invalid"))
+}
+
 fn main() {
     let args = ServerArgs::from_args();
 
-    let log_level = args.logging;
-    TermLogger::init(log_level, Config::default(), TerminalMode::Stderr)
+    TermLogger::init(args.logging, Config::default(), TerminalMode::Stderr)
         .expect("Error while initializing of TermLogger");
+    debug!("Conf: {:?}", args);
 
     let current_dir = env::current_dir()
         .expect("Can not get current dir");
 
-    let addr = args.addr;
+    let engine_file = current_dir.join(ENGINE_PATH);
+    match current_engine(&engine_file) {
+        Some(engine) => {
+            if engine != args.engine {
+                error!("Storage directory is already powered by other engine: {}, new one: {}",
+                       engine,
+                       args.engine);
+                exit(-1);
+            }
+            debug!("Engine file: {}", engine);
+        },
+        None => {
+            debug!("Set new engine: {}", args.engine);
+            std::fs::write(&engine_file,
+                           format!("{}", args.engine))
+                .expect("Error writing to engine file");
+        }
+    }
 
     let engine: Result<Box<dyn KvsEngine>> = match args.engine {
         Engine::Kvs => KvStore::open(current_dir).map(|x| Box::new(x) as _),
         Engine::Sled => SledEngine::open(current_dir).map(|x| Box::new(x) as _),
     };
-
     let mut engine = engine.expect("Can not open KvsEngine");
 
-    if let Err(e) = Server::new(addr).run(engine.as_mut()) {
+    let server = Server::new(args.addr);
+    if let Err(e) = server.run(engine.as_mut()) {
         error!("{}", e);
     }
 }
